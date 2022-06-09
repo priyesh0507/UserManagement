bcf6e69d3e4966872a535f9caa4716cc31eeeb8a
priyesh0507 <102280332+priyesh0507@users.noreply.github.com>
2022-06-09 17:09:46 +0530

    deleting parent directory

--- user-management/src/main/java/com/gemalto/cota/repo/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gemalto.cota.repo;
-
-import com.gemalto.cota.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
