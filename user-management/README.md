c06f8193ad54c3c53a5048028a952b1ba12cd6af
priyesh0507 <102280332+priyesh0507@users.noreply.github.com>
2022-06-09 12:52:32 +0530

    user management added
    dockerfile +deployment.yaml added

---
+++ user-management/README.md
@@ -0,0 +1,12 @@
+# Spring REST Hello World Example
+
+Article link : https://www.mkyong.com/spring-boot/user-management/
+
+## 1. How to start
+```
+$ git clone https://github.com/ashu32tech/spring-boot.git
+$ cd user-management
+$ mvn spring-boot:run
+
+$ curl -v localhost:8080/users
+```
---
+++ user-management/src/main/java/com/gemalto/cota/entities/UserRole.java
@@ -0,0 +1,24 @@
+package com.gemalto.cota.entities;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class UserRole {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String roleName;
+    @OneToMany(mappedBy = "role")
+    private List<UserRoleToPrivilege> userRoleToPrivileges;
+}
+
