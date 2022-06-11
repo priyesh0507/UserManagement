976992dece89dae3f4d11304ef1b64e4eb0ee803
priyesh0507 <102280332+priyesh0507@users.noreply.github.com>
2022-06-11 12:15:27 +0530

    Delete src directory

--- src/main/java/com/gemalto/cota/error/UserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.gemalto.cota.error;
-
-public class UserNotFoundException extends RuntimeException {
-
-    public UserNotFoundException(Long id) {
-        super("Book id not found : " + id);
-    }
-
-}
